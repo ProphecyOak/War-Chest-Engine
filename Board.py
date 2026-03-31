d9186913575d21b8bff74c1d03a971594de9d4b2
ProphecyOak <drito610@gmail.com>
2026-03-31 06:45:30 -0400

    Hex Offset?

--- Board.py
+++ Board.py
@@ -1,8 +1,12 @@
 class Board():
 	def __init__(self):
 		self.dim = {"height": 1, "width": 1}
-		self.offset = {"x": 0, "y": 0}
-		self.tiles = [["*"]]
+		self.offset = {
+			"x": 0, "y": 0,
+			"q": 0, "r": 0
+		}
+		self.tiles = [[None]]
+		self[self.offset["q"],self.offset["r"]] = "*"
 	
 	def __getitem__(self, qr):
 		x, y = self.axial_to_xy(qr)
@@ -20,26 +24,28 @@ class Board():
 				self.offset["x"] -= 1
 				for j in range(self.dim["height"]):
 					self.tiles[j].insert(0,None)
-			self.dim["width"] += 1
+				self.dim["width"] += 1
 		if y < 0:
 			for j in range(-y):
 				self.offset["y"] -= 1
 				self.tiles.insert(0, [None for i in range(self.dim["width"])])
-			self.dim["height"] += 1
+				self.dim["height"] += 1
 		if x >= self.dim["width"]:
 			for i in range(x - self.dim["width"] + 1):
 				for j in range(self.dim["height"]):
 					self.tiles[j].append(None)
-			self.dim["width"] += 1
+				self.dim["width"] += 1
 		if y >= self.dim["height"]:
 			for j in range(y - self.dim["height"] + 1):
 				self.tiles.append([None for i in range(self.dim["width"])])
-			self.dim["height"] += 1
+				self.dim["height"] += 1
 		x, y = self.axial_to_xy(qr)
 		self.tiles[y][x] = value
 
 	def axial_to_xy(self, qr):
 		q, r = qr
+		q -= self.offset["q"]
+		r -= self.offset["r"]
 		parity = q&1
 		col = q - self.offset["x"]
 		row = r + (q - parity) // 2 - self.offset["y"]
@@ -50,28 +56,34 @@ class Board():
 		x -= self.offset["x"]
 		y -= self.offset["y"]
 		parity = x&1
-		q = x
-		r = y - (x - parity) // 2
+		q = x - self.offset["q"]
+		r = y - (x - parity) // 2 - self.offset["r"]
 		return (q, r)
 	
 	def __str__(self):
 		return "\n".join(map(lambda row: " ".join([f"{str(tile):5}" for tile in row]), self.tiles))
 	
-	def print_as_hexes(self):
+	def hexes_str(self):
+		hex_width = 9
+		spacing = " " * hex_width
+		rows = []
 		for y in range(self.dim["height"] * 2):
+			rows.append([])
 			parity = y&1
 			true_y = y // 2
-			if parity: print("      ",end="")
-			for x in range(0 if parity else 1, self.dim["width"], 2):
+			if parity: rows[-1].append("")
+			for x in range(1 if parity else 0, self.dim["width"], 2):
 				# tile_rep = str(self.tiles[true_y][x])
-				tile_rep = f"{x},{true_y}"
-				print(f"{tile_rep:>12}",end="")
-			print()
+				q, r = self.xy_to_axial((x, true_y))
+				tile_rep = f"{q:^2}/{r:^2}/{-q-r:^2}"
+				rows[-1].append(f"{tile_rep:>{hex_width}}")
+			if parity: rows[-1].append("")
+
+		return "\n".join([spacing.join(row) for row in rows])
 	
 if __name__ == "__main__":
 	my_board = Board()
-	for q in range(2):
-		for r in range(2):
+	for q in range(3):
+		for r in range(3):
 			my_board[q, r] = True
-			my_board.print_as_hexes()
-			print("------------")
+	print(my_board.hexes_str())
